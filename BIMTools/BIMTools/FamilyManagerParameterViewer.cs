cc1f0559353d8b9d5be3c6e1ba4c279b49ce4a74
kWork <kolya0714@yandex.ru>
2024-02-27 18:43:38 +0300

    +

---
+++ BIMTools/BIMTools/FamilyManagerParameterViewer.cs
@@ -0,0 +1,30 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIMTools
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    internal class FamilyManagerParameterViewer : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+            Autodesk.Revit.ApplicationServices.Application app = commandData.Application.Application;
+
+            var fmDoc = doc.FamilyManager;
+            var window = new FamilyManagerParameterViewerWindow();
+            window.familyManager = fmDoc;
+
+            window.Show();
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ BIMTools/BIMTools/FamilyManagerParameterViewerWindow.cs
@@ -0,0 +1,42 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BIMTools
+{
+    public partial class FamilyManagerParameterViewerWindow : System.Windows.Forms.Form
+    {
+        public FamilyManagerParameterViewerWindow()
+        {
+            InitializeComponent();
+        }
+
+        public FamilyManager familyManager { get; set; }
+
+        private void FamilyManagerParameterViewerWindow_Load(object sender, EventArgs e)
+        {
+            var column = new DataGridViewColumn();
+            column.HeaderText = "Параметры";
+            column.CellTemplate = new DataGridViewTextBoxCell();
+            dataGridView1.Columns.Add(column);
+
+            var types = familyManager.Types.Cast<FamilyType>().ToArray();
+            var parameters = familyManager.Parameters.Cast<FamilyParameter>().Where(p => p.IsShared);
+            foreach (var type in types)
+            {
+                column = new DataGridViewColumn();
+                column.HeaderText = type.Name;
+                column.Name = type.Name;
+                column.CellTemplate = new DataGridViewTextBoxCell();
+                dataGridView1.Columns.Add(column);
+            }
+        }
+    }
+}
