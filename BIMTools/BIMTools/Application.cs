2681adc4f50bd5e701252e66ce26955a00dc8c02
Nick Home <kolya0714@yandex.ru>
2024-03-07 08:33:34 +0300

    +

--- BIMTools/BIMTools/Application.cs
+++ BIMTools/BIMTools/Application.cs
@@ -30,6 +30,7 @@ namespace BIMTools
             AddButton(ribbonPanel, "Копировать общие параметры", assemblyPath, $"{nameof(BIMTools)}.{nameof(BIMTools.CopyParametersFromSample)}", "Копировать с другого открытого семейства общие параметры", "icons8-алмазный-уход-24");
             AddButton(ribbonPanel, "Посмотреть значения типоразмеров", assemblyPath, $"{nameof(BIMTools)}.{nameof(BIMTools.FamilyManagerParameterViewer)}", "Откроет окно значений типоразмеров семейства", "icons8-алмазный-уход-24");
             AddButton(ribbonPanel, "Инженерные пересечения", assemblyPath, $"{nameof(BIMTools)}.{nameof(BIMTools.FindIntersections)}", "Находит самопересечения между инженерными коммуникациями", "icons8-intersect-24");
+            AddButton(ribbonPanel, "Создать матрицу элементов", assemblyPath, $"{nameof(BIMTools)}.{nameof(BIMTools.PlaceFamilyInstances)}", "Создает матрицу элементов", "icons8-matrix-24");
             AddButton(ribbonPanel, "CloseOtherDocuments", assemblyPath, $"{nameof(BIMTools)}.{nameof(BIMTools.CloseOtherProjects)}", "Закрывает не активные документы", "CloseOtherProjects24");
 
             return Result.Succeeded;
